f923f12d56461d4528fe76503ecf7534607cf55f
Marc-Aurel Zent <marc_aurel@me.com>
2023-04-02 16:13:10 +0200

    refactor Handlers into IPC and Websocket implementations

--- IINACT/IpcProviders.cs
+++ IINACT/IpcProviders.cs
@@ -6,18 +6,16 @@ namespace IINACT;
 
 internal class IpcProviders : IDisposable
 {
-    private readonly DalamudPluginInterface dalamudPluginInterface;
-
-    internal static Version IpcVersion => new(2, 0, 0);
+    internal static Version IpcVersion => new(2, 1, 0);
     internal readonly ICallGateProvider<Version> GetVersion;
     internal readonly ICallGateProvider<Version> GetIpcVersion;
 
-    private event EventHandler<string> OnSendMessageOverIpcEvent;
-    private readonly List<string> ipcMessageEndpoints = new ();
-
     public RainbowMage.OverlayPlugin.WebSocket.ServerController? Server { get; set; }
-    internal readonly ICallGateProvider<string, bool> SubscribeToCombatEvents;
-    internal readonly ICallGateProvider<string, bool> UnsubscribeFromCombatEvents;
+    public RainbowMage.OverlayPlugin.Handlers.Ipc.IpcHandlerController? OverlayIpcHandler { get; set; }
+
+    internal readonly ICallGateProvider<string, bool> CreateSubscriber;
+    internal readonly ICallGateProvider<string, bool> CreateLegacySubscriber;
+    internal readonly ICallGateProvider<string, bool> Unsubscribe;
     internal readonly ICallGateProvider<bool> GetServerRunning;
     internal readonly ICallGateProvider<int> GetServerPort;
     internal readonly ICallGateProvider<string> GetServerIp;
@@ -26,14 +24,13 @@ internal class IpcProviders : IDisposable
     
     internal IpcProviders(DalamudPluginInterface pluginInterface)
     {
-        dalamudPluginInterface = pluginInterface;
-        OnSendMessageOverIpcEvent += SendMessageOverIpc;
-
         GetVersion = pluginInterface.GetIpcProvider<Version>("IINACT.Version");
         GetIpcVersion = pluginInterface.GetIpcProvider<Version>("IINACT.IpcVersion");
         
-        SubscribeToCombatEvents = pluginInterface.GetIpcProvider<string, bool>("IINACT.Server.SubscribeToCombatEvents");
-        UnsubscribeFromCombatEvents = pluginInterface.GetIpcProvider<string, bool>("IINACT.Server.UnsubscribeFromCombatEvents");
+        CreateSubscriber = pluginInterface.GetIpcProvider<string, bool>("IINACT.CreateSubscriber");
+        CreateLegacySubscriber = pluginInterface.GetIpcProvider<string, bool>("IINACT.CreateLegacySubscriber");
+        Unsubscribe = pluginInterface.GetIpcProvider<string, bool>("IINACT.Unsubscribe");
+        
         GetServerRunning = pluginInterface.GetIpcProvider<bool>("IINACT.Server.Listening");
         GetServerPort = pluginInterface.GetIpcProvider<int>("IINACT.Server.Port");
         GetServerIp = pluginInterface.GetIpcProvider<string>("IINACT.Server.Ip");
@@ -47,32 +44,10 @@ internal class IpcProviders : IDisposable
     {
         GetVersion.RegisterFunc(() => Assembly.GetExecutingAssembly().GetName().Version!);
         GetIpcVersion.RegisterFunc(() => IpcVersion);
-
-        SubscribeToCombatEvents.RegisterFunc(
-            specifiedEndpoint =>
-            {
-                if (Server == null) return false;
-                if (ipcMessageEndpoints.Count < 1 && Server.SubscribeToIpcSession(OnSendMessageOverIpcEvent))
-                {
-                    ipcMessageEndpoints.Add(specifiedEndpoint);
-                    return true;
-                }
-
-                return false;
-            }
-        );
-        UnsubscribeFromCombatEvents.RegisterFunc(
-            specifiedEndpoint =>
-            {
-                var success = ipcMessageEndpoints.Remove(specifiedEndpoint);
-                if (ipcMessageEndpoints.Count < 1)
-                {
-                    Server?.UnsubscribeFromIpcSession();
-                }
-
-                return success;
-            }
-        );
+        
+        CreateSubscriber.RegisterFunc(name => OverlayIpcHandler?.CreateSubscriber(name) ?? false);
+        CreateLegacySubscriber.RegisterFunc(name => OverlayIpcHandler?.CreateLegacySubscriber(name) ?? false);
+        Unsubscribe.RegisterFunc(name => OverlayIpcHandler?.Unsubscribe(name) ?? false);
 
         GetServerRunning.RegisterFunc(() => Server?.Running ?? false);
         GetServerPort.RegisterFunc(() => Server?.Port ?? 0);
@@ -81,22 +56,14 @@ internal class IpcProviders : IDisposable
         GetServerUri.RegisterFunc(() => Server?.Uri);
     }
 
-    private void SendMessageOverIpc(object? o, string message)
-    {
-        foreach (var endpoint in ipcMessageEndpoints)
-        {
-            dalamudPluginInterface.GetIpcSubscriber<string, bool>(endpoint).InvokeFunc(message);
-        }
-    }
-
     public void Dispose()
     {
         GetVersion.UnregisterFunc();
         GetIpcVersion.UnregisterFunc();
-
-        Server?.UnsubscribeFromIpcSession();
-        SubscribeToCombatEvents.UnregisterFunc();
-        UnsubscribeFromCombatEvents.UnregisterFunc();
+        
+        CreateSubscriber.UnregisterFunc();
+        CreateLegacySubscriber.UnregisterFunc();
+        Unsubscribe.UnregisterFunc();
 
         GetServerRunning.UnregisterFunc();
         GetServerPort.UnregisterFunc();
---
+++ OverlayPlugin.Core/Handlers/Ipc/IHandlerFactory.cs
@@ -0,0 +1,39 @@
+using Dalamud.Plugin;
+using Dalamud.Plugin.Ipc;
+using Newtonsoft.Json.Linq;
+
+namespace RainbowMage.OverlayPlugin.Handlers.Ipc;
+
+internal interface IHandlerFactory
+{
+    IHandler Create(string name, TinyIoCContainer container)
+    {
+        var pluginInterface = container.Resolve<DalamudPluginInterface>();
+        var receiver = pluginInterface.GetIpcProvider<JObject, bool>($"IINACT.IpcProvider.{name}");
+        var sender = pluginInterface.GetIpcSubscriber<JObject, bool>(name);
+        var logger = container.Resolve<ILogger>();
+        var dispatcher = container.Resolve<EventDispatcher>();
+        var repository = container.Resolve<FFXIVRepository>();
+        return CreateHandler(name, receiver, sender, logger, dispatcher, repository);
+    }
+
+    protected IHandler CreateHandler(
+        string name, ICallGateProvider<JObject, bool> receiver, ICallGateSubscriber<JObject, bool> sender,
+        ILogger logger, EventDispatcher eventDispatcher, FFXIVRepository repository);
+}
+
+internal class HandlerFactory : IHandlerFactory
+{
+    public IHandler CreateHandler(
+        string name, ICallGateProvider<JObject, bool> receiver, ICallGateSubscriber<JObject, bool> sender,
+        ILogger logger, EventDispatcher eventDispatcher, FFXIVRepository repository) =>
+        new IpcHandler(name, receiver, sender, logger, eventDispatcher);
+}
+
+internal class LegacyHandlerFactory : IHandlerFactory
+{
+    public IHandler CreateHandler(
+        string name, ICallGateProvider<JObject, bool> receiver, ICallGateSubscriber<JObject, bool> sender,
+        ILogger logger, EventDispatcher eventDispatcher, FFXIVRepository repository) =>
+        new LegacyIpcHandler(name, receiver, sender, logger, eventDispatcher, repository);
+}

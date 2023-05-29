3112b5955dd60682dc97c61f533af5a4120e95a3
Marc-Aurel Zent <marc_aurel@me.com>
2023-05-29 13:23:08 +0200

    rework FFXIV_ACT_Plugin memory reads

--- FetchDependencies/Patcher.cs
+++ FetchDependencies/Patcher.cs
@@ -115,7 +115,90 @@ internal class Patcher
             var ilProcessor = method.Body.GetILProcessor();
             ilProcessor.Replace(0, Instruction.Create(OpCodes.Ret));
         }
+        
+        {
+            var method = memory.GetMethod(
+                "System.Void FFXIV_ACT_Plugin.Memory.ScanMemory::Run(System.Object)");
+            var ilProcessor = method.Body.GetILProcessor();
+            ilProcessor.Replace(16, Instruction.Create(OpCodes.Ldc_I4_S, (sbyte)10));
+        }
+        
+        var marshallType = memory.Assembly.MainModule.ImportReference(typeof(System.Runtime.InteropServices.Marshal)).Resolve();
+        var marshallCopyBuffer = marshallType.Methods.First(
+            m => m.FullName ==
+                 "System.Void System.Runtime.InteropServices.Marshal::Copy(System.IntPtr,System.Byte[],System.Int32,System.Int32)");
+        var marshallCopyBufferReference = memory.Assembly.MainModule.ImportReference(marshallCopyBuffer);
 
+        void UseMarshallCopyBuffer(MethodDefinition method)
+        {
+            var originalIl = method.Body.Instructions.ToArray();
+            var bufferIndex =
+                Array.FindIndex(
+                    originalIl,
+                    code => code.OpCode == OpCodes.Callvirt && code.Operand.ToString()!.Contains("ReadBuffer"));
+            var dynamicLength = originalIl[bufferIndex - 1].OpCode == OpCodes.Conv_I4;
+            var offset = dynamicLength ? -3 : 0;
+            var ilProcessor = method.Body.GetILProcessor();
+            ilProcessor.Replace(bufferIndex, Instruction.Create(OpCodes.Call, marshallCopyBufferReference));
+            ilProcessor.InsertAfter(bufferIndex - 2 + offset, Instruction.Create(OpCodes.Ldc_I4_0));
+            ilProcessor.RemoveAt(bufferIndex - 6 + offset);
+            ilProcessor.RemoveAt(bufferIndex - 6 + offset);
+        }
+
+        var readBufferMethods = new[]
+        {
+            "System.IntPtr FFXIV_ACT_Plugin.Memory.MemoryReader.ReadParty::Read()",
+            "System.IntPtr FFXIV_ACT_Plugin.Memory.MemoryReader.ReadZoneMap::Read()",
+            "System.IntPtr FFXIV_ACT_Plugin.Memory.MemoryReader.ReadCombatant::Read(System.IntPtr)",
+            "System.IntPtr FFXIV_ACT_Plugin.Memory.MemoryReader.ReadPlayer::Read()",
+            "System.IntPtr FFXIV_ACT_Plugin.Memory.MemoryReader.ReadMobArray::Read64()"
+        };
+
+        foreach (var methodName in readBufferMethods)
+            UseMarshallCopyBuffer(memory.GetMethod(methodName));
+        
+        var intPtrType = memory.Assembly.MainModule.ImportReference(typeof(IntPtr)).Resolve();
+        var intPtrOp =
+            intPtrType.Methods.First(m => m.FullName == "System.Void* System.IntPtr::op_Explicit(System.IntPtr)");
+        var intPtrOpReference = memory.Assembly.MainModule.ImportReference(intPtrOp);
+        
+        {
+            var method = memory.GetMethod(
+                "System.UInt32 FFXIV_ACT_Plugin.Memory.MemoryReader.ReadMemory::ReadUInt32(System.IntPtr)");
+            var ilProcessor = method.Body.GetILProcessor();
+            ilProcessor.Clear();
+            ilProcessor.Emit(OpCodes.Ldarg_1);
+            ilProcessor.Emit(OpCodes.Call, intPtrOpReference);
+            ilProcessor.Emit(OpCodes.Ldind_U4);
+            ilProcessor.Emit(OpCodes.Ret);
+        }
+        
+        {
+            var method = memory.GetMethod(
+                "System.UInt64 FFXIV_ACT_Plugin.Memory.MemoryReader.ReadMemory::ReadUInt64(System.IntPtr)");
+            var ilProcessor = method.Body.GetILProcessor();
+            ilProcessor.Clear();
+            ilProcessor.Emit(OpCodes.Ldarg_1);
+            ilProcessor.Emit(OpCodes.Call, intPtrOpReference);
+            ilProcessor.Emit(OpCodes.Ldind_I8);
+            ilProcessor.Emit(OpCodes.Ret);
+        }
+        
+        {
+            var method = memory.GetMethod(
+                "System.IntPtr FFXIV_ACT_Plugin.Memory.MemoryReader.ReadMemory::ReadPointer(System.IntPtr)");
+            var ilProcessor = method.Body.GetILProcessor();
+            ilProcessor.Clear();
+            ilProcessor.Emit(OpCodes.Ldarg_1);
+            ilProcessor.Emit(OpCodes.Call, intPtrOpReference);
+            ilProcessor.Emit(OpCodes.Ldind_I8);
+            var intPtrCtor =
+                intPtrType.Methods.First(m => m.FullName == "System.Void System.IntPtr::.ctor(System.Int64)");
+            var intPtrCtorReference = memory.Assembly.MainModule.ImportReference(intPtrCtor);
+            ilProcessor.Emit(OpCodes.Newobj, intPtrCtorReference);
+            ilProcessor.Emit(OpCodes.Ret);
+        }
+        
         memory.WriteOut();
     }
 }

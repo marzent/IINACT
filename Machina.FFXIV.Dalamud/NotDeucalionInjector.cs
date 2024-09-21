892941a1d0733e05e465c9483df9523bec1d5511
Menci <mencici@msn.com>
2024-09-21 14:08:39 +0800

    Update NotDeucalionInjector

--- Machina.FFXIV.Dalamud/NotDeucalionInjector.cs
+++ Machina.FFXIV.Dalamud/NotDeucalionInjector.cs
@@ -2,12 +2,29 @@ namespace Machina.FFXIV.Dalamud
 {
     public static class NotDeucalionInjector
     {
+        public static string LastInjectionError
+        {
+            get
+            {
+                return string.Empty;
+            }
+            set
+            {
+                // Do nothing
+            }
+        }
+
+        public static bool ValidateLibraryChecksum()
+        {
+            return true;
+        }
+
         public static string ExtractLibrary()
         {
             return string.Empty;
         }
 
-        public static bool InjectLibrary(int processId, string deucalionPath)
+        public static bool InjectLibrary(int processId)
         {
             return true;
         }

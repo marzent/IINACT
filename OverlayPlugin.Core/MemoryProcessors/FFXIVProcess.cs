ba0d0173ee9964a1f77165d4d677b8a6676dad6b
Marc-Aurel Zent <mzent@codeweavers.com>
2025-03-30 12:38:24 +0200

    remove unused ToProperCase helper

--- OverlayPlugin.Core/MemoryProcessors/FFXIVProcess.cs
+++ OverlayPlugin.Core/MemoryProcessors/FFXIVProcess.cs
@@ -121,13 +121,6 @@ namespace RainbowMage.OverlayPlugin.MemoryProcessors
                    job == EntityJob.CUL;
         }
 
-        static internal string ToProperCase(string name)
-        {
-            System.Globalization.CultureInfo cultureInfo = System.Threading.Thread.CurrentThread.CurrentCulture;
-            System.Globalization.TextInfo textInfo = cultureInfo.TextInfo;
-            return textInfo.ToTitleCase(name);
-        }
-
         [Serializable]
         public class EntityData
         {

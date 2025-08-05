6d73f441f8597a92c757f8d5e8ddcbfcae230989
Marc-Aurel Zent <mzent@codeweavers.com>
2025-08-06 01:26:28 +0200

    add Google TTS fallback implementation

--- IINACT/TextToSpeechProvider.cs
+++ IINACT/TextToSpeechProvider.cs
@@ -1,15 +1,14 @@
-using System.Diagnostics;
 using System.Speech.Synthesis;
-using System.Text.RegularExpressions;
+using System.Web;
+using NAudio.Wave;
 
 namespace IINACT;
 
 internal class TextToSpeechProvider
 {
-    private string binary = "/usr/bin/say";
-    private string args = "";
     private readonly object speechLock = new();
-    private readonly SpeechSynthesizer? speechSynthesizer;  
+    private readonly HttpClient client = new();
+    private readonly SpeechSynthesizer? speechSynthesizer;
     
     public TextToSpeechProvider()
     {
@@ -21,6 +20,7 @@ internal class TextToSpeechProvider
         catch (Exception ex)
         {
             Plugin.Log.Warning(ex, "Failed to initialize SAPI TTS engine");
+            speechSynthesizer = null;
         }
         
         Advanced_Combat_Tracker.ActGlobals.oFormActMain.TextToSpeech += Speak;
@@ -30,47 +30,44 @@ internal class TextToSpeechProvider
     {
         Task.Run(() =>
         {
-            if (new FileInfo(binary).Exists)
-            {
-                try
-                {
-                    var ttsProcess = new Process
-                    {
-                        StartInfo =
-                        {
-                            FileName = "C:\\windows\\system32\\start.exe",
-                            CreateNoWindow = true,
-                            UseShellExecute = false,
-                            Arguments = $"/unix {binary} {args} \"" +
-                                        Regex.Replace(Regex.Replace(message, @"(\\*)" + "\"", @"$1$1\" + "\""),
-                                                      @"(\\+)$", @"$1$1") + "\""
-                        }
-                    };
-                    lock (speechLock)
-                    {
-                        ttsProcess.Start();
-                        // heuristic pause
-                        Thread.Sleep(500 * message.Split(' ', StringSplitOptions.RemoveEmptyEntries).Length);
-                    }
-                
-                    return;
-                }
-                catch (Exception ex)
-                {
-                    Plugin.Log.Error(ex, $"TTS failed to play back {message}");
-                    return;
-                }
-            }
-        
             try
             {
-                lock (speechLock)
-                    speechSynthesizer?.Speak(message);
+                if (speechSynthesizer == null)
+                    SpeakGoogle(message);
+                else
+                    SpeakSapi(message);
             }
             catch (Exception ex)
             {
                 Plugin.Log.Error(ex, $"TTS failed to play back {message}");
             }
         });
+    }
+
+    private void SpeakGoogle(string message)
+    {
+        var query = HttpUtility.UrlEncode(message);
+        const string lang = "en";
+        var url = $"https://translate.google.com/translate_tts?ie=UTF-8&client=tw-ob&tl={lang}&q={query}";
+        var mp3Data = client.GetByteArrayAsync(url).Result;
+
+        using var stream = new MemoryStream(mp3Data);
+        using var reader = new Mp3FileReader(stream);
+        using var waveOut = new WaveOutEvent();
+        waveOut.Init(reader);
+        var waitHandle = new ManualResetEventSlim(false);
+        
+        lock (speechLock)
+        {
+            waveOut.Play();
+            waveOut.PlaybackStopped += (s, e) => waitHandle.Set();
+            waitHandle.Wait();
+        }
+    }
+    
+    private void SpeakSapi(string message)
+    {
+        lock (speechLock)
+            speechSynthesizer?.Speak(message);
     }
 }

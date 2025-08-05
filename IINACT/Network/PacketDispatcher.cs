529a7b4539344f856a90c227999420772bc8a7e4
Marc-Aurel Zent <mzent@codeweavers.com>
2025-08-05 14:01:20 +0200

    add PacketDispatcher

---
+++ IINACT/Network/PacketDispatcher.cs
@@ -0,0 +1,26 @@
+﻿namespace IINACT.Network;
+
+public unsafe struct PacketDispatcher
+{
+    private void* Unknown1;
+    private void* Unknown2;
+    private void* NetworkModuleProxy;
+
+    public uint GameRandom;
+    public uint LastPacketRandom;
+    public uint Key0;
+    public uint Key1;
+    public uint Key2;
+    public uint Unknown_32;
+
+    public static PacketDispatcher* GetInstance()
+    {
+        var framework = FFXIVClientStructs.FFXIV.Client.System.Framework.Framework.Instance();
+        if (framework == null) return null;
+        var nmp = framework->NetworkModuleProxy;
+        if (nmp == null) return null;
+        var rcb = nmp->ReceiverCallback;
+        if (rcb == null) return null;
+        return (PacketDispatcher*)&rcb->PacketDispatcher;
+    }
+}
